3daa9eee430d6062d2a71de2959bed21ab9e6ea8
odinfiller <nevetssav@verizon.net>
2024-09-02 23:00:09 -0400

    rps 1.0

--- RPS_js.js
+++ RPS_js.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    choices = ["Rock","Paper","Scissors"]
+    choice = Math.round(Math.random() * 3)
+    return(choices[choice])
+};
+
+function getHumanChoice(){
+    choice = prompt("R, P, S?")
+    return(choice)
+}
+
+console.log(getComputerChoice)
+console.log(getHumanChoice)
